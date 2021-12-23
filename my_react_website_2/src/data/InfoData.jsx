b501cf603d5406f84469006890c779178a68ce0e
Amber M <amberm31@protonmail.com>
2021-12-22 21:03:04 -0600

    added info section to homepage

---
+++ my_react_website_2/src/data/InfoData.jsx
@@ -0,0 +1,29 @@
+/** @format */
+
+// Vertical Images
+import ImageOne from '../images/house-8-vertical.jpeg';
+import ImageTwo from '../images/house-9-vertical.jpeg';
+
+export const InfoData = {
+  heading: 'Explore our beautiful homes',
+  paragraphOne:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias minima fugit esse nam consectetur sapiente voluptatibus porro quibusdam corporis obcaecati?',
+  paragraphTwo:
+    'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore ullam quisquam assumenda numquam optio. At quia facilis iusto nesciunt deserunt. Iste quisquam repudiandae suscipit quod numquam officiis cupiditate mollitia ipsam.',
+  buttonLabel: 'View Homes',
+  image: ImageOne,
+  reverse: false,
+  delay: 100,
+};
+
+export const InfoDataTwo = {
+  heading: 'Modern Designs',
+  paragraphOne:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias minima fugit esse nam consectetur sapiente voluptatibus porro quibusdam corporis obcaecati?',
+  paragraphTwo:
+    'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore ullam quisquam assumenda numquam optio. At quia facilis iusto nesciunt deserunt. Iste quisquam repudiandae suscipit quod numquam officiis cupiditate mollitia ipsam.',
+  buttonLabel: 'View Homes',
+  image: ImageTwo,
+  reverse: true,
+  delay: 300,
+};
